f4fcdb2d95f147b973579405c50dc429f77c6dd4
SesisnandoLRNeto <nandolrneto@gmail.com>
2023-03-19 10:59:30 -0400

    feat(mobile): Cadastro implementado

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,10 +2,37 @@ import React, { useRef } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import LogoComponent from './components/Logo';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
 
 const HomeScreen = () => {
-  const pagerRef = useRef(null);
   const navigation = useNavigation();
+
+  const provider = new GoogleAuthProvider();
+
+  const pagerRef = useRef(null);
+
+  const googleSignIn = async () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(token, user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
 
   return (
     <View style={styles.container}>
@@ -24,10 +51,10 @@ const HomeScreen = () => {
         >
           Como deseja entrar?
         </Text>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity onPress={googleSignIn} style={styles.button}>
           <Text style={styles.buttonText}>Continue com sua conta Google</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity onPress={googleSignIn} style={styles.button}>
           <Text style={styles.buttonText}>Continue com sua conta Facebook</Text>
         </TouchableOpacity>
         <TouchableOpacity
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,17 +1,71 @@
 import { useNavigation } from '@react-navigation/native';
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  updateProfile,
+} from 'firebase/auth';
 import React, { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { TextInput, HelperText, Text, Divider } from 'react-native-paper';
+import {
+  Alert,
+  Image,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Divider, HelperText, Text, TextInput } from 'react-native-paper';
+import { auth } from '../../firebaseConfig';
 import BackButton from './components/BackButton';
 
 const SignupScreen = () => {
+  const navigation = useNavigation();
+  const logo = require('../../assets/adaptive-icon.png');
+
+  const provider = new GoogleAuthProvider();
+
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [pass, setPass] = useState('');
-  const [confirmPass, setconfirmPass] = useState('');
+  const [confirmPass, setConfirmPass] = useState('');
 
-  const navigation = useNavigation();
-  const logo = require('../../assets/adaptive-icon.png');
+  const invalidButton = () => {
+    return (
+      (name.length < 8 && name !== '') ||
+      (pass !== '' && pass.length < 8) ||
+      (confirmPass !== '' && confirmPass.length < 8) ||
+      (!email.includes('@') && email !== '')
+    );
+  };
+
+  const signUp = async () => {
+    if (!invalidButton()) {
+      await createUserWithEmailAndPassword(auth, email, pass)
+        .then(() => {
+          updateProfile(auth.currentUser, {
+            displayName: name,
+          })
+            .then(() => {
+              Alert.alert('Sucesso', 'Usuário foi criado', [
+                {
+                  text: 'Ok',
+                  onPress: () => navigation.navigate('Login'),
+                },
+              ]);
+
+              setName('');
+              setEmail('');
+              setPass('');
+              setConfirmPass('');
+            })
+            .catch((error) => {
+              Alert.alert(error.code, error.message);
+            });
+        })
+        .catch((error) => {
+          Alert.alert('Error', error.message);
+        });
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -51,7 +105,7 @@ const SignupScreen = () => {
               style={styles.input}
               label='Name'
               placeholder='Name'
-              value={email}
+              value={name}
               onChangeText={(name) => setName(name)}
             />
             <HelperText type='error' visible={name.length < 8 && name !== ''}>
@@ -77,6 +131,7 @@ const SignupScreen = () => {
               label='Senha'
               placeholder='Senha'
               value={pass}
+              secureTextEntry
               right={<TextInput.Icon icon='eye' />}
               onChangeText={(pass) => setPass(pass)}
             />
@@ -89,7 +144,8 @@ const SignupScreen = () => {
               mode='outlined'
               label='Confirmar senha'
               placeholder='Confirmar senha'
-              value={pass}
+              secureTextEntry
+              value={confirmPass}
               right={<TextInput.Icon icon='eye' />}
               onChangeText={(confirmPass) => setConfirmPass(confirmPass)}
             />
@@ -99,7 +155,10 @@ const SignupScreen = () => {
             >
               Sua senha deve ter mais do que 8 caracteres!
             </HelperText>
-            <HelperText type='error' visible={confirmPass !== pass}>
+            <HelperText
+              type='error'
+              visible={confirmPass !== pass && confirmPass !== ''}
+            >
               As senhas não são iguais!
             </HelperText>
           </View>
@@ -123,10 +182,7 @@ const SignupScreen = () => {
         }}
       >
         <View style={styles.buttonContainerLogin}>
-          <TouchableOpacity
-            onPress={() => navigation.navigate('Home')}
-            style={styles.button}
-          >
+          <TouchableOpacity onPress={signUp} style={styles.button}>
             <Text style={styles.buttonText}>Sign Up</Text>
           </TouchableOpacity>
         </View>
@@ -194,6 +250,7 @@ const styles = StyleSheet.create({
   },
   input: {
     minWidth: '80%',
+    minHeight: '10%',
   },
 });
 
